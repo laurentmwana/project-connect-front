21229aac715cfdf45b0712bc2daeed041cc27f66
impulse <ephraimimpulse17@gmail.com>
2025-05-15 18:59:17 +0100

    feature:creation projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
+import CreateComponent from './pages/project/create/create.component' ;
+
 
 export const routes: Routes = [
+  {
+    path: 'project/create',
+    component: CreateComponent,
+  },
 ];
