b51ad22c7cf6738d407ecb3a368e7deaf648067e
Labeya <laurentmwn@gmail.com>
2025-05-23 17:16:38 +0100

    feat: ajout d'un guard pour empêcher l'accès aux pages publiques si l'utilisateur est connecté (/login, /forgot-password, etc.)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ForgotComponent } from './pages/auth/forgot/forgot.component';
 import { PasswordResetComponent } from './pages/auth/password-reset/password-reset.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { VerifyEmailComponent } from './pages/auth/verify-email/verify-email.component';
+import { guestGuard } from './guards/guest.guard';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,7 @@ export const routes: Routes = [
           {
             path: 'create',
             component: CreateComponent,
+            title: "Création d'un projet - ProjectConnect",
           },
         ],
       },
@@ -32,25 +34,31 @@ export const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent,
+        title: 'Se connecter - ProjectConnect',
       },
       {
         path: 'forgot-password',
         component: ForgotComponent,
+        title: 'Mot de passe oublié - ProjectConnect',
       },
       {
         path: 'password-reset/:token',
         component: PasswordResetComponent,
+        title: 'Réinitialisation du mot de passe - ProjectConnect',
       },
 
       {
         path: 'register',
         component: RegisterComponent,
+        title: 'Création du compte - ProjectConnect',
       },
 
       {
         path: 'verify-email',
         component: VerifyEmailComponent,
+        title: "Vérification de l'adresse e-mail - ProjectConnect",
       },
     ],
+    canActivate: [guestGuard],
   },
 ];
