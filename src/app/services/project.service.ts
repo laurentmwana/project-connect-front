10fd7b20fcda1f8277b392d2121f89c24ea7aa1a
JeremieMatondo <makondelejeremie60@gmail.com>
2025-05-29 13:57:28 +0100

    modification du service

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
-
-import { Observable } from 'rxjs';
 import { Project, ProjectData } from '../model/project';
 import { Domain } from '@/model/domain';
 import { Role } from '@/model/role';
 import { Skill } from '@/model/skill';
-import { Environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectService {
-  // URL de base de l'API
-  private url = 'http://127.0.0.1:8000/api/projects';
+  // ✅ URL de base corrigée (pas de doublon 'projects')
+  private baseUrl = 'http://127.0.0.1:8000/api/';
 
   constructor(private http: HttpClient) {}
 
@@ -22,44 +20,45 @@ export class ProjectService {
    * Récupère la liste des projets
    */
   getAllProjects(): Observable<ProjectData[]> {
-    return this.http.get<ProjectData[]>(this.url + 'projects');
+    return this.http
+      .get<{ data: ProjectData[] }>(this.baseUrl + 'projects')
+      .pipe(
+        map((response) => response.data) // <- extrait le tableau depuis `data`
+      );
   }
 
   /**
    * Crée un nouveau projet
    */
   createProject(data: Project): Observable<any> {
-    return this.http.post(this.url + 'projects', data);
+    return this.http.post(this.baseUrl + 'projects', data);
   }
 
   /**
    * Récupère un projet par ID
    */
   getProjectById(id: string): Observable<ProjectData> {
-    return this.http.get<ProjectData>(`${this.url}projects/${id}`);
+    return this.http.get<ProjectData>(this.baseUrl + 'projects/' + id);
   }
 
   /**
    * Récupère les domaines disponibles
    */
   getAvailableDomains(): Observable<{ data: Domain[] }> {
-    return this.http.get<{ data: Domain[] }>(this.url + 'domains');
+    return this.http.get<{ data: Domain[] }>(this.baseUrl + 'domains');
   }
 
   /**
    * Récupère les rôles disponibles
    */
   getAvailableRoles(): Observable<{ data: Role[] }> {
-    return this.http.get<{ data: Role[] }>(this.url + 'roles');
+    return this.http.get<{ data: Role[] }>(this.baseUrl + 'roles');
   }
 
   /**
    * Récupère les compétences disponibles
    */
   getAvaillableSkills(): Observable<{ data: Skill[] }> {
-    return this.http.get<{ data: Skill[] }>(this.url + 'skills');
+    return this.http.get<{ data: Skill[] }>(this.baseUrl + 'skills');
   }
 }
-
- 
-
