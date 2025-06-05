43dcef699031a4c508c5d53d0c0fd9bcadf20996
impulse <ephraimimpulse17@gmail.com>
2025-06-05 17:42:38 +0100

    Creation des methodes pour postuler a un role d'un projet  et pour visualisation des candidatures par le créateur du projet

--- src/app/pages/project/project-detail/project-detail.component.ts
+++ src/app/pages/project/project-detail/project-detail.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavbarComponent } from '../../../components/navbar/navbar.component';
 import { CandidacyService } from '@/services/candidacy.service';
+import { Candidacy, Meta } from '@/model/candidacy';
+import { UserLocalService } from '@/services/user-local.service';
 
 @Component({
   selector: 'app-project-detail',
@@ -16,13 +18,23 @@ export class ProjectDetailComponent {
   projectId!: string;
   project!: ProjectData;
   errorMessage = '';
+  meta: Meta | null = null;
+  candidacies: Candidacy[] = [];
+  currentPage: number = 1;
+  isOwner: boolean = false;
+
+  // Filtres
+  roleFilter: string = '';
+  userFilter: string = '';
+  validatedFilter: number = -1;
 
   toasts: { message: string; type: 'success' | 'error' }[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private projectService: ProjectService,
-    private candidacyService: CandidacyService
+    private candidacyService: CandidacyService,
+    private userLocalService: UserLocalService
   ) {}
 
   message: string = '';
@@ -32,10 +44,39 @@ export class ProjectDetailComponent {
     this.projectService.getProjectById(this.projectId).subscribe({
       next: (response) => {
         this.project = response.data;
-        console.log(this.project); // Pour vérifier que les données sont là
+
+        const user = this.userLocalService.getUser();
+        this.isOwner = user?.id === this.project.created_by.id;
+        console.log(this.isOwner);
+
+        if (this.isOwner) {
+          this.loadCandidacies(Number(this.projectId));
+        }
       },
       error: () => (this.errorMessage = 'Erreur lors du chargement du projet'),
     });
+  }
+
+  loadCandidacies(projectId: number) {
+    this.candidacyService
+      .getCandidacies(+this.projectId, {
+        role_name: this.roleFilter,
+        user_name: this.userFilter,
+        is_validated:
+          this.validatedFilter >= 0 ? this.validatedFilter : undefined,
+        per_page: 10,
+        page: this.currentPage,
+      })
+      .subscribe({
+        next: (response) => {
+          this.candidacies = response.data;
+          console.log(this.candidacies, 'fonction loadCandidacies');
+          this.meta = response.meta;
+        },
+        error: () => {
+          this.showToast('Erreur lors du chargement des candidatures', 'error');
+        },
+      });
   }
 
   onApplyRole(id: number) {
@@ -45,7 +86,6 @@ export class ProjectDetailComponent {
       },
       error: (err) => {
         let error = err.error;
-
         this.showToast(`${error.message}`, 'error');
       },
     });
