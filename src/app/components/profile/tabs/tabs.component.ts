1d2991a09e1c30494e4b2de093339d61d57975d5
line <linemangoma@gmail.com>
2025-06-16 12:49:18 +0100

    suppression et appel des nouvelles tabs about me, experience, portfolio, recommandation

--- src/app/components/profile/tabs/tabs.component.ts
+++ src/app/components/profile/tabs/tabs.component.ts
@@ -1,45 +1,20 @@
-import { Experience } from '@/model/experience';
-import { Portfolio } from '@/model/portfolio';
-import { ExperienceService } from '@/services/experience.service';
-import { PortfolioService } from '@/services/portfolio.service';
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { AboutMeComponent } from './about_me/about-me/about-me.component';
+import { ExperienceComponent } from './experience/experience/experience.component';
+import { PortfolioComponent } from './portfolio/portfolio/portfolio.component';
+import { RecommandationComponent } from './recommandation/recommandation/recommandation.component';
 
 @Component({
   selector: 'app-tabs',
-  imports: [NgFor],
+  imports: [
+    AboutMeComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    RecommandationComponent,
+  ],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.css',
 })
 export class TabsComponent {
- 
-  constructor(private portofolioService: PortfolioService, private experienceService : ExperienceService) {}
-
-  portfolios!: Portfolio[];
-   experiences!: Experience[];
-  ngOnInit() {
-    this.getMyPortofolios();
-    this.getExperiences();
-  }
-
-  getMyPortofolios() {
-    this.portofolioService.getMyPortofolios().subscribe({
-      next: (response) => {
-        this.portfolios = response.data;
-        console.log('message dans la console', this.portfolios);
-      },
-      error: (err) => {
-        console.log(err.error);
-      },
-    });
-  }
-
-  getExperiences(){
-    this.experienceService.getExperiences().subscribe({
-      next : (data)=> {
-        this.experiences = data.data;
-        console.log('Messaged dans fjkfkfk', this.experiences);
-      }
-    })
-  }
+  constructor() {}
 }
