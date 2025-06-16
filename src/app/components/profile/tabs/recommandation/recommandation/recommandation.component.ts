3962563c9a676b6d8be0ed0c099d7196077d9995
line <linemangoma@gmail.com>
2025-06-16 12:55:10 +0100

    creation du tabs recommandation

---
+++ src/app/components/profile/tabs/recommandation/recommandation/recommandation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommandation',
+  imports: [],
+  templateUrl: './recommandation.component.html',
+  styleUrl: './recommandation.component.css'
+})
+export class RecommandationComponent {
+
+}
