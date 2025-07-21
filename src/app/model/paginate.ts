dc24621702e648278c40cf38ef932b1b0bfeb21b
Labeya <laurentmwn@gmail.com>
2025-07-21 11:28:34 +0100

    création du composant pagination + model paginate et création d'un service pour fusionner les query params de l'url

---
+++ src/app/model/paginate.ts
@@ -0,0 +1,27 @@
+export interface PaginationLink {
+  url: string | null;
+  label: string;
+  active: boolean;
+}
+
+export interface PaginationMeta {
+  current_page: number;
+  from: number | null;
+  last_page: number;
+  links: PaginationLink[];
+  path: string;
+  per_page: number;
+  to: number | null;
+  total: number;
+}
+
+export interface PaginationDataResponse<T> {
+  data: T;
+  links: {
+    first: string | null;
+    last: string | null;
+    prev: string | null;
+    next: string | null;
+  };
+  meta: PaginationMeta;
+}
