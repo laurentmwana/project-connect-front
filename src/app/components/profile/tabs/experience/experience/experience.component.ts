35de0ae2541b10ad552032512634e24922b5343e
line <linemangoma@gmail.com>
2025-07-22 17:05:01 +0100

    ajout du bouton ajouter une expérience et du routerlink pour acceder au formulaire des experiences dans experience.component.html et les importations dans sont composant Ts

--- src/app/components/profile/tabs/experience/experience/experience.component.ts
+++ src/app/components/profile/tabs/experience/experience/experience.component.ts
@@ -2,15 +2,19 @@ import { Experience } from '@/model/experience';
 import { ExperienceService } from './../../../../../services/experience.service';
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ExperienceformComponent } from '../experienceform/experienceform.component';
+import { inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-experience',
-  imports: [NgFor],
+  imports: [NgFor,RouterLink],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
   constructor(private experienceService: ExperienceService){}
+
 
   experiences!:Experience[];
 ngOnInit(){
@@ -21,9 +25,10 @@ ngOnInit(){
     this.experienceService.getExperiences().subscribe({
       next : (data)=> {
         this.experiences = data.data;
-        console.log('Messaged dans fjkfkfk', this.experiences);
       }
     })
   }
+
+
 
 }
