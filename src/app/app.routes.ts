32d3506bfd7a72666f1755a7a666376116cf8f1f
line <linemangoma@gmail.com>
2025-08-05 17:19:25 +0100

    suppression de la route d'experince dans app/routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,7 +22,6 @@ import { AdminComponent } from './pages/admin/admin/admin.component';
 import { adminGuard } from './guards/admin.guard';
 import { NotificationDetailComponent } from './pages/notification/notification-detail/notification-detail.component';
 import { NotificationListComponent } from './pages/notification/notification-list/notification-list.component';
-import { ExperienceformComponent } from './components/profile/tabs/experience/experienceform/experienceform.component';
 import { PortfolioformComponent } from './components/profile/tabs/portfolio/portfolioform/portfolioform.component';
 export const routes: Routes = [
   {
@@ -52,12 +51,6 @@ export const routes: Routes = [
         component: ProjectEditComponent,
         title: 'Modifier un projet - ProjectConnect',
         canActivate: [canEditGuard],
-      },
-      {
-        path: 'experience/create',
-        component: ExperienceformComponent,
-        title: 'Ajouter une Experience',
-        canActivate: [protectedGuard],
       },
 
       {
